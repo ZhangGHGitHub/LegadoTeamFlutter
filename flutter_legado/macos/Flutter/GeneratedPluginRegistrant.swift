a1c21e9fd5be9db12b7adfa0188b14ae2f668d07
ZhangGHGitHub <512824117@qq.com>
2026-08-25 20:56:05 +0800

    chore(ui): macOS GeneratedPluginRegistrant 补 package_info_plus 注册
    与 pubspec 依赖（package_info_plus ^9.0.0）同步的 Flutter 工具生成文件。

--- flutter_legado/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_legado/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import file_picker
 import mobile_scanner
+import package_info_plus
 import share_plus
 import shared_preferences_foundation
 import sqflite_darwin
@@ -17,6 +18,7 @@ import webview_flutter_wkwebview
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
